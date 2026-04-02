dffd834cd73f8b5356d7aabfa539133476a79a94
Avids <sina.ahmadi@gmail.com>
2026-04-02 13:38:55 -0400

    Update pdf.ts

--- src/lib/pdf.ts
+++ src/lib/pdf.ts
@@ -1,5 +1,7 @@
 import * as pdfjsLib from "pdfjs-dist";
-if (pdfjsLib?.GlobalWorkerOptions) pdfjsLib.GlobalWorkerOptions.workerSrc = "";
+import workerSrc from "pdfjs-dist/build/pdf.worker.min?url";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
 
 export async function renderSinglePagePdfToCanvas(file: File, scale = 2.2) {
   const bytes = new Uint8Array(await file.arrayBuffer());
